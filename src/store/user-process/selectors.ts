ed64e1fd74917e008a4e7408dd34e5401ccfe138
Валерий Мащенко <walera.99@mail.ru>
2024-11-17 23:25:26 +0500

    Исправляет замечания

--- src/store/user-process/selectors.ts
+++ src/store/user-process/selectors.ts
@@ -3,7 +3,5 @@ import { State } from '../../types/state.ts';
 
 export const getAuthorizationStatus = (state: State): AuthorizationStatus =>
   state[NameSpace.User].authorizationStatus;
-export const getIsUserAuthenticated = (state: State): boolean =>
-  state[NameSpace.User].authorizationStatus === AuthorizationStatus.Auth;
 export const getUserData = (state: State) => state[NameSpace.User].userData;
 export const getIsLoginLoadingStatus = (state: State): boolean => state[NameSpace.User].isLoadingStatus;
